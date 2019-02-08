9077260138c2091518983c0a84a062bbff680e29
Roger Butenuth <rogerbutenuth@gmx.de>
2019-02-08 16:15:23 +0100

    Fixed scope bug.

--- src/main/java/de/codecentric/fpl/builtin/Assignment.java
+++ src/main/java/de/codecentric/fpl/builtin/Assignment.java
@@ -17,41 +17,45 @@ import de.codecentric.fpl.datatypes.Symbol;
 public class Assignment {
 
 	/**
-	 * @param scope
-	 *            Scope to which functions should be added.
-	 * @throws ScopeException
-	 *             Should not happen on initialization.
+	 * @param scope Scope to which functions should be added.
+	 * @throws ScopeException Should not happen on initialization.
 	 */
 	public static void put(Scope scope) throws ScopeException {
 
 		scope.put(new AssignmentFunction("put",
-				comment("Assign symbol to evluated value in current scope, deletes if value is null"), "symbol", "value") {
+				comment("Assign symbol to evluated value in current scope, deletes if value is null"), "symbol",
+				"value") {
 			@Override
-			public FplValue callInternal(Scope scope, FplValue[] parameters) throws EvaluationException {
-				String name = targetName(scope, parameters[0]);
-				try {
-					scope.put(name, value(scope, parameters[1]));
-				} catch (ScopeException e) {
-					throw new EvaluationException(e);
-				}
-				return value(scope, parameters[1]);
+			protected void scopeAction(Scope scope, String name, FplValue value) throws ScopeException {
+				scope.put(name, value);
 			}
 		});
 
 		scope.put(new AssignmentFunction("put-global",
-				comment("Assign symbol to evluated value in global scope, deletes if value is null"), "symbol", "value") {
+				comment("Assign symbol to evluated value in global scope, deletes if value is null"), "symbol",
+				"value") {
 			@Override
-			public FplValue callInternal(Scope scope, FplValue[] parameters) throws EvaluationException {
-				String name = targetName(scope, parameters[0]);
-				try {
-					scope.putGlobal(name, value(scope, parameters[1]));
-				} catch (ScopeException e) {
-					throw new EvaluationException(e);
-				}
-				return value(scope, parameters[1]);
+			protected void scopeAction(Scope scope, String name, FplValue value) throws ScopeException {
+				scope.putGlobal(name, value);
 			}
 		});
 
+		scope.put(new AssignmentFunction("set", comment("Reassign value in scope chain. nil as value not allowed"),
+				"symbol", "value") {
+			@Override
+			protected void scopeAction(Scope scope, String name, FplValue value) throws ScopeException {
+				scope.change(name, value);
+			}
+		});
+
+		scope.put(new AssignmentFunction("def",
+				comment("Assign value in local scope, it must be unassigned before. nil as value not allowed"),
+				"symbol", "value") {
+			@Override
+			protected void scopeAction(Scope scope, String name, FplValue value) throws ScopeException {
+				scope.define(name, value);
+			}
+		});
 	}
 
 	private static abstract class AssignmentFunction extends Function {
@@ -60,6 +64,16 @@ public class Assignment {
 			super(name, comment, false, parameterNames);
 		}
 
+		@Override
+		public FplValue callInternal(Scope scope, FplValue[] parameters) throws EvaluationException {
+			try {
+				scopeAction(scope, targetName(scope, parameters[0]), value(scope, parameters[1]));
+			} catch (ScopeException e) {
+				throw new EvaluationException(e);
+			}
+			return value(scope, parameters[1]);
+		}
+		
 		protected String targetName(Scope scope, FplValue expression) throws EvaluationException {
 			if (expression == null) {
 				throw new EvaluationException("nil not valid name for assignment");
@@ -78,5 +92,7 @@ public class Assignment {
 		protected FplValue value(Scope scope, FplValue expression) throws EvaluationException {
 			return expression == null ? null : expression.evaluate(scope);
 		}
+		
+		protected abstract void scopeAction(Scope scope, String name, FplValue value) throws ScopeException;
 	}
 }
