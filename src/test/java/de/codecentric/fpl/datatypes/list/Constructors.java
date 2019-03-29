90a225b181a86951dab6e20a78ec8abdd5f82e7b
Roger Butenuth <rogerbutenuth@gmx.de>
2019-03-29 12:40:44 +0100

    Tests for constructor from Iterator.

--- src/test/java/de/codecentric/fpl/datatypes/list/Constructors.java
+++ src/test/java/de/codecentric/fpl/datatypes/list/Constructors.java
@@ -5,10 +5,13 @@ import static org.junit.Assert.assertFalse;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 import org.junit.Test;
 
 import de.codecentric.fpl.EvaluationException;
+import de.codecentric.fpl.datatypes.FplInteger;
 import de.codecentric.fpl.datatypes.FplValue;
 
 public class Constructors extends AbstractListTest {
@@ -76,25 +79,63 @@ public class Constructors extends AbstractListTest {
 		new FplList(values, bucketSizes);
 	}
 	
-	private SizedIterator<FplValue> createIterator(int from, int to) {
-		return new SizedIterator<FplValue>() {
+	@Test
+	public void smallFromStandardIterator() throws Exception {
+		int from = 0;
+		int to = 8;
+		FplList list = FplList.fromIterator(new Iterator<FplValue>() {
+			int nextValue = from;
 			
 			@Override
 			public FplValue next() {
-				// TODO Auto-generated method stub
-				return null;
+				if (hasNext()) {
+					return FplInteger.valueOf(nextValue++);
+				} else {
+					throw new NoSuchElementException();
+				}
 			}
 			
 			@Override
 			public boolean hasNext() {
-				// TODO Auto-generated method stub
-				return false;
+				return nextValue < to;
+			}
+		});
+		check(0, 8, list);
+	}
+	
+	@Test
+	public void smallFromIterator() throws Exception {
+		FplList list = FplList.fromIterator(createIterator(0, 8));
+		check(0, 8, list);
+	}
+	
+	@Test
+	public void largeFromIterator() throws Exception {
+		FplList list = FplList.fromIterator(createIterator(0, 100));
+		check(0, 100, list);
+	}
+	
+	private SizedIterator<FplValue> createIterator(int from, int to) {
+		return new SizedIterator<FplValue>() {
+			int nextValue = from;
+			
+			@Override
+			public FplValue next() {
+				if (hasNext()) {
+					return FplInteger.valueOf(nextValue++);
+				} else {
+					throw new NoSuchElementException();
+				}
+			}
+			
+			@Override
+			public boolean hasNext() {
+				return nextValue < to;
 			}
 			
 			@Override
 			public int size() {
-				// TODO Auto-generated method stub
-				return 0;
+				return to - from;
 			}
 		};
 	}
