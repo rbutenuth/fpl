96fb71ccd9f735d12729a5a18ec05a16a87b95e5
Roger Butenuth <roger.butenuth@codecentric.de>
2020-07-11 11:31:42 +0200

    Replaced reader two handle BOM.

--- src/main/java/de/codecentric/fpl/builtin/InputOutput.java
+++ src/main/java/de/codecentric/fpl/builtin/InputOutput.java
@@ -1,10 +1,8 @@
 package de.codecentric.fpl.builtin;
 
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.URI;
 import java.net.URISyntaxException;
