4e9833fde720f3109cc8922ac3626602555ccd47
roger.butenuth <roger.butenuth@codecentric.de>
2021-10-01 12:24:26 +0200

    Alternate implementation of flatMap, but less efficient.

--- src/test/java/de/codecentric/fpl/datatypes/list/MapTest.java
+++ src/test/java/de/codecentric/fpl/datatypes/list/MapTest.java
@@ -1,5 +1,7 @@
 package de.codecentric.fpl.datatypes.list;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.util.function.Function;
 
 import org.junit.jupiter.api.Test;
@@ -25,8 +27,21 @@ public class MapTest extends AbstractListTest {
 	}
 
 	@Test
+	public void flatMapEmptyList() throws EvaluationException {
+		FplList list = FplList.EMPTY_LIST;
+		list = list.flatMap(new Function<FplValue, FplList>() {
+
+			@Override
+			public FplList apply(FplValue t) {
+				return (FplList)t;
+			}
+		});
+		assertTrue(list.isEmpty());
+	}
+	
+	@Test
 	public void flatMap() throws EvaluationException {
-		FplList list = FplList.fromValues(create(0, 3), create(3, 7));
+		FplList list = FplList.fromValues(create(0, 3), FplList.EMPTY_LIST, create(3, 7));
 		list = list.flatMap(new Function<FplValue, FplList>() {
 
 			@Override
